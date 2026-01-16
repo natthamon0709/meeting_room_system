e8134ba623f69dcaec4ac122c181eda561fc296f
natthamon0 <natthamon.k@merudy.com>
2026-01-16 09:08:16 +0700

    Add initial implementation of meeting room booking system with dashboard, database setup, and management features

---
+++ database.sql
@@ -0,0 +1,11 @@
+
+CREATE DATABASE meeting CHARACTER SET utf8mb4;
+USE meeting;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(50)
+);
+
+INSERT INTO users (username,password) VALUES ('admin','1234');
