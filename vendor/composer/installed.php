95754506a70896f29a3e73a09529085e384fcd96
natthamon0 <natthamon.k@merudy.com>
2026-01-22 17:44:48 +0700

    Add Composer dependencies and autoloading files
    - Created composer.lock to lock project dependencies.
    - Added vendor/autoload.php for autoloading classes.
    - Introduced vendor/composer/ClassLoader.php for PSR-0, PSR-4, and classmap loading.
    - Implemented vendor/composer/InstalledVersions.php to track installed packages.
    - Added LICENSE file for Composer's licensing information.
    - Generated autoloading files: autoload_classmap.php, autoload_namespaces.php, autoload_psr4.php, autoload_real.php, and autoload_static.php.
    - Created installed.json and installed.php to manage installed packages.
    - Added platform_check.php to verify PHP version compatibility.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cf211957e787339f8ae7fa54d472396de36a4f5a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cf211957e787339f8ae7fa54d472396de36a4f5a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
